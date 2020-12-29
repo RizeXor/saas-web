edbc01afaf3a849323db642e37f714d3148902e9
RizeXor <decodestudios.help@gmail.com>
2020-12-29 08:50:50 +0530

    Added shared types & seperate SubscriptionComponent

--- src/components/Subscription.tsx
+++ src/components/Subscription.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import dayjs from 'dayjs';
+import { Subscription } from '../types/subscription';
 
-const SubscriptionComponent = () => {
+type SubscriptionComponentProps = {
+  sub: Subscription
+};
+
+const SubscriptionComponent: React.FC<SubscriptionComponentProps> = ({ sub: { plan, current_period_end, current_period_start, days_until_due, status } }) => {
   return (
-    <div>
-
+    <div className="col-lg-4" key={plan.product.id}>
+      <div className="card rounded">
+        {plan.product.images[0] && <img src={plan.product.images[0]} alt={plan.product.name + " image"} className="card-img-top img-fluid" />}
+        <div className="card-body">
+          <h5 className="card-title">{plan.product.name + " "}
+            <span className={status === "active" ? "badge badge-success" : "badge badge-danger"}>
+              {status.charAt(0).toUpperCase() + status.slice(1)}
+            </span>
+          </h5>
+          <p className="card-text">
+            Billed: {plan.currency === 'usd' ? '$' : plan.currency}{plan.amount.toString().slice(0, -2) + "." + plan.amount.toString().slice(-2)} every {plan.interval}
+          </p>
+        </div>
+        <div className="card-footer text-white-50 d-flex justify-content-between">
+          <small>Expires on {dayjs.unix(current_period_end).format("DD MMM YYYY")}</small>
+          <small>{days_until_due} {days_until_due === 1 ? "day" : "days"} left</small>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,27 +2,8 @@ import React from 'react';
 import { useQuery } from 'react-query';
 import axios, { AxiosError } from 'axios';
 import dayjs from 'dayjs';
-
-type Product = {
-  id: string,
-  created: number,
-  name: string
-};
-
-type Plan = {
-  amount: number,
-  interval: string,
-  currency: string,
-  product: Product
-};
-
-type Subscription = {
-  current_period_end: number,
-  current_period_start: number,
-  days_until_due: number,
-  status: string,
-  plan: Plan
-};
+import SubscriptionComponent from '../components/Subscription';
+import { Subscription } from '../types/subscription';
 
 const fetchSubscriptions = async (): Promise<Subscription[]> => {
   const res = await axios.get('/api/v1/subscriptions', {
@@ -48,25 +29,8 @@ const HomePage = () => {
     return <span>Error: {error?.message}</span>
   }
 
-  const listSubscriptions = data?.map(sub => (
-    <div className="col-sm-4" key={sub.plan.product.id}>
-      <div className="card shadow rounded">
-        <div className="card-body">
-          <h5 className="card-title">{sub.plan.product.name + " "}
-            <span className={sub.status === "active" ? "badge badge-success" : "badge badge-danger"}>
-              {sub.status.charAt(0).toUpperCase() + sub.status.slice(1)}
-            </span>
-          </h5>
-          <p className="card-text">
-            Billed: {sub.plan.currency === 'usd' ? '$' : sub.plan.currency}{sub.plan.amount.toString().slice(0, -2) + "." + sub.plan.amount.toString().slice(-2)} every {sub.plan.interval}, {sub.days_until_due} days left
-          </p>
-        </div>
-        <div className="card-footer text-white-50">
-          <small>Expires on {dayjs.unix(sub.current_period_end).format("DD MMM YYYY")}</small>
-        </div>
-      </div>
-    </div>
-  ));
+  const listSubscriptions = data?.map(sub =>
+    <SubscriptionComponent key={sub.plan.product.id} sub={sub} />);
 
   return (
     <div className="container mt-3">
